1836470eb33eaf13c08f30b11035bd49194276b1
Akshat1308 <suranaas@rknec.edu>
2023-12-13 23:22:02 +0530

    .

---
+++ interview prep/Tree/maximumPathSum.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Definition for a binary tree node
+struct TreeNode
+{
+    int data;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int val)
+    {
+        data = val;
+        left = right = NULL;
+    }
+};
+int main()
+{
+    // Example usage:
+    // Creating a simple binary tree
+    TreeNode *root = new TreeNode(1);
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(3);
+    root->left->left = new TreeNode(4);
+    root->left->right = new TreeNode(5);
+}
