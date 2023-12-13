184487b2165ae9e602c804f44854c4e78b80edb9
Akshat1308 <suranaas@rknec.edu>
2023-12-13 23:49:48 +0530

    maxPath

--- interview prep/Tree/maximumPathSum.cpp
+++ interview prep/Tree/maximumPathSum.cpp
@@ -13,6 +13,22 @@ struct TreeNode
         left = right = NULL;
     }
 };
+int maxPathSum(TreeNode *root, int &maxi)
+{
+    if (root == NULL)
+        return 0;
+    int maxLeft = maxPathSum(root->left, maxi);
+    int maxRight = maxPathSum(root->right, maxi);
+    maxi = max(maxi, maxLeft + maxRight + root->data);
+    return root->data + max(maxLeft ,maxRight);
+}
+int callerFunction(TreeNode *root)
+{
+    int answer;
+    maxPathSum(root, answer);
+    return answer;
+}
+
 int main()
 {
     // Example usage:
@@ -22,4 +38,5 @@ int main()
     root->right = new TreeNode(3);
     root->left->left = new TreeNode(4);
     root->left->right = new TreeNode(5);
+    cout << callerFunction(root);
 }
